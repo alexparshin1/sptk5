fed625cedc9158ed404b005ac78839497ba6fc90
Alexey Parshin <alexeyp@gmail.com>
2012-03-24 19:00:00 +1100

    Added driver load test

---
+++ examples/database/driver_load_test.cpp
@@ -0,0 +1,38 @@
+/***************************************************************************
+                          SIMPLY POWERFUL TOOLKIT (SPTK)
+                               DEMO PROGRAMS SET
+                          driver_load_test.cpp  -  description
+                             -------------------
+    begin                : Mar 24, 2012
+    copyright            : (C) 2000-2012 by Alexey S.Parshin
+    email                : alexeyp@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifdef __BORLANDC__
+#include <vcl.h>
+#pragma hdrstop
+#endif
+
+#include <iostream>
+#include <iomanip>
+
+#include <sptk5/db/CODBCDatabase.h>
+#include <sptk5/cdatabase>
+#include <sptk5/cutils>
+
+using namespace std;
+using namespace sptk;
+
+int main()
+{
+    return 0;
+}
