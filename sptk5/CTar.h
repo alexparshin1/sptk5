92e4f43dc4bd6c00bd0bc610d42f1be3a5fc7f02
Alexey Parshin <alexeyp@gmail.com>
2016-06-12 16:20:26 +1000

    Replaced Copyright header on *.cpp and *.h files

--- sptk5/CTar.h
+++ sptk5/CTar.h
@@ -1,32 +1,30 @@
-/***************************************************************************
-                          SIMPLY POWERFUL TOOLKIT (SPTK)
-                          CTar.h  -  description
-                             -------------------
-    begin                : Fri Sep 1 2006
-    copyright            : (C) 1999-2014 by Alexey Parshin. All rights reserved.
-
-    This module creation was sponsored by Total Knowledge (http://www.total-knowledge.com).
-    Author thanks the developers of CPPSERV project (http://www.total-knowledge.com/progs/cppserv)
-    for defining the requirements for this class.
- ***************************************************************************/
-
-/***************************************************************************
-   This library is free software; you can redistribute it and/or modify it
-   under the terms of the GNU Library General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or (at
-   your option) any later version.
-
-   This library is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library
-   General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public License
-   along with this library; if not, write to the Free Software Foundation,
-   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
-
-   Please report all bugs and problems to "alexeyp@gmail.com"
- ***************************************************************************/
+/*
+╔══════════════════════════════════════════════════════════════════════════════╗
+║                       SIMPLY POWERFUL TOOLKIT (SPTK)                         ║
+║                       CTar.h - description                                   ║
+╟──────────────────────────────────────────────────────────────────────────────╢
+║  begin                Thursday May 25 2000                                   ║
+║  copyright            (C) 1999-2016 by Alexey Parshin. All rights reserved.  ║
+║  email                alexeyp@gmail.com                                      ║
+╚══════════════════════════════════════════════════════════════════════════════╝
+┌──────────────────────────────────────────────────────────────────────────────┐
+│   This library is free software; you can redistribute it and/or modify it    │
+│   under the terms of the GNU Library General Public License as published by  │
+│   the Free Software Foundation; either version 2 of the License, or (at your │
+│   option) any later version.                                                 │
+│                                                                              │
+│   This library is distributed in the hope that it will be useful, but        │
+│   WITHOUT ANY WARRANTY; without even the implied warranty of                 │
+│   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library   │
+│   General Public License for more details.                                   │
+│                                                                              │
+│   You should have received a copy of the GNU Library General Public License  │
+│   along with this library; if not, write to the Free Software Foundation,    │
+│   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.               │
+│                                                                              │
+│   Please report all bugs and problems to alexeyp@gmail.com.                  │
+└──────────────────────────────────────────────────────────────────────────────┘
+*/
 
 #ifndef __CTAR_H__
 #define __CTAR_H__
