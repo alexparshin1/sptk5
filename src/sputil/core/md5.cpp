94c66b77a305cb06d08e403757e7078f80fd35bf
Alexey Parshin <alexeyp@gmail.com>
2018-05-05 09:48:40 +1000

    Fixed bug in MD5 class

--- src/sputil/core/md5.cpp
+++ src/sputil/core/md5.cpp
@@ -338,7 +338,7 @@ std::string MD5::hexdigest() const
   if (!finalized)
     return "";
 
-  char buf[33];
+  char buf[34];
   for (int i = 0; i < 16; i++)
     snprintf(buf + i * 2, sizeof(buf) - i * 2 - 1, "%02x", digest[i]);
   buf[32] = 0;
