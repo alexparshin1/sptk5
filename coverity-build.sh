e0b970ff5bd50c1b71337935752a0e11d631ab5b
Alexey Parshin <alexeyp@gmail.com>
2018-10-20 09:24:30 +1100

    Added coverity build script

---
+++ coverity-build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#sysctl vsyscall=emulate
+
+make clean
+
+cov-build --dir cov-int make -j 8
+rc=$?
+if [ $rc != 0 ]; then
+    tail cov-int/build-log.txt
+    exit 1
+fi
+
+exit 1
+
+tar czvf upload/sptk.tgz cov-int
+
+curl --form token=uRFUaePWkebk8cxkxbeUCg \
+  --form email=alexeyp@gmail.com \
+  --form file=@upload/sptk.tgz \
+  --form version="5.2.14" \
+  --form description="SPTK" \
+  -o coverity-upload.log --progress-bar \
+  https://scan.coverity.com/builds?project=SPTK
