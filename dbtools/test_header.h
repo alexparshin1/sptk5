d3d5c80a4cbcec84ab3680b37afc947287663231
Alexey Parshin <alexeyp@gmail.com>
2018-11-08 19:02:04 +1100

    File attributes changed

--- dbtools/test_header.h
+++
@@ -1,9 +0,0 @@
-/***************************************************************************
-                          Test Project 
-                          @filename@  -  description
-                          -------------------------
-    created               : @date@
-    copyright             : (C) 1999-2014 by Test Developer
-    author                : Test Developer
-    email                 : test@testhost.com
- ***************************************************************************/
