39ff9eb0045331ac4fe427f91d5bc0b406322997
Alexey Parshin <alexeyp@gmail.com>
2019-05-19 17:47:06 +1000

    Fixed debian package build script

--- build/build-debian-package.sh
+++ build/build-debian-package.sh
@@ -4,12 +4,20 @@ export PATH=/opt/sonar-scanner/bin:$PATH
 
 which debuild > /dev/null || sudo apt install build-essential devscripts debhelper
 
-VERSION="5.2.13"
+VERSION="5.2.19"
+
+BUILD_HOME=$(pwd)
+cd ../code || exit 1
+
+./distclean.sh
 
 cmake . -DCMAKE_INSTALL_PREFIX=/usr
 
 cd ..
-tar zcf sptk_$VERSION.orig.tar.gz sptk5 || exit 1
-cd sptk5/code || exit 1
+tar zcf sptk_${VERSION}.orig.tar.gz code || exit 1
+cd code || exit 1
 
+pwd
 debuild -us -uc -b
+
+cd ${BUILD_HOME}
