89075c9f42f4973d9d35d9609e68ccaeffae9153
Alexey Parshin <alexeyp@gmail.com>
2019-05-31 21:13:16 -0700

    Updated Windows auto-build: - Added installer generation - Added auto-upload to sptk.net

--- build/build_windows.bat
+++ build/build_windows.bat
@@ -1,14 +1,49 @@
-cd "C:\Users\alexe\workspace\sptk5\build\test"
+@echo off
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+cd "C:\Users\alexe\workspace\sptk5\build" 2>&1 > build.log
 
-mkdir build64
-rem del /S /Q build64\*.*
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" 2>&1 >> build.log
+if errorlevel 1 (
+    type build.log
+    exit /b %errorlevel%
+)
+
+mkdir build64 2>&1 >> build.log
 cd build64
+if errorlevel 1 (
+    echo "Can't switch to build directory"
+    exit /b %errorlevel%
+)
 
-cmake -G "Visual Studio 16 2019" -A x64 -DUSE_GTEST=OFF "C:\Users\alexe\workspace\sptk5\code"
+rem cmake -G "Visual Studio 16 2019" -A x64 -DUSE_GTEST=OFF "C:\Users\alexe\workspace\sptk5\code"
+if errorlevel 1 (
+    echo "Can't configure build"
+    exit /b %errorlevel%
+)
 cd ..
 
-cmake --build build64 --config Release --target INSTALL
+rem cmake --build build64 --config Release --target INSTALL
+if errorlevel 1 (
+    echo "Can't complete build and install"
+    exit /b %errorlevel%
+)
 
-REM devenv ..\code\w64\SPTK.sln /build RelWithDebInfo /project INSTALL
+"C:\Program Files (x86)\Caphyon\Advanced Installer 16.0\bin\x86\advinst.exe" /build SPTK.aip 2>&1 >> build.log
+if errorlevel 1 (
+    echo "Can't build installer"
+    exit /b %errorlevel%
+)
+
+mkdir Downloads 2>&1 >> build.log
+
+set /p VERSION=<VERSION
+
+mv SPTK-SetupFiles\SPTK.exe Downloads\SPTK-%VERSION%.exe 2>&1 >> build.log
+if errorlevel 1 (
+    echo "Can't move installer to Downloads directory"
+    exit /b %errorlevel%
+)
+
+rmdir /S /Q SPTK-SetupFiles SPTK-cache 2>&1 >> build.log
+
+scp -i ssh\id_rsa Downloads\SPTK-5.2.19.exe alexeyp@protis-demo.com:/var/www/sites/sptk/download/SPTK-5.2.19.exe
