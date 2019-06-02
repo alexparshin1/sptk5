b0ad59ad0d1eae0d700881801b2a17bc95d842bf
Alexey Parshin <alexeyp@gmail.com>
2019-06-02 00:11:01 +0000

    MC

--- build/build-debian-package.sh
+++ build/build-debian-package.sh
@@ -11,7 +11,7 @@ cd ../code || exit 1
 
 ./distclean.sh
 
-cmake . -DCMAKE_INSTALL_PREFIX=/usr -DUSE_GTEST=NO
+cmake . -DCMAKE_INSTALL_PREFIX=/usr -DUSE_GTEST=OFF -DBUILD_EXAMPLES=OFF
 
 cd ..
 tar zcf sptk_${VERSION}.orig.tar.gz code || exit 1
