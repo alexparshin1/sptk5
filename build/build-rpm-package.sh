72b1b8925a174af65cd0a5fa61e75a85e5222a7f
Alexey Parshin <alexeyp@gmail.com>
2019-06-04 05:54:05 +0000

    Debugging RPM build script

--- build/build-rpm-package.sh
+++ build/build-rpm-package.sh
@@ -1,34 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
-VERSION=$(head -1 VERSION)
+VERSION=$(head -1 /build/scripts/VERSION)
 RELEASE="1"
 PACKAGE_NAME="SPTK-$VERSION"
 
 CORES=$(grep processor /proc/cpuinfo | wc -l)
 CORES=$(($CORES-1))
 
-cat sptk5.spec.src | sed -r "s/\{\{CORES\}\}/$CORES/" | sed -r "s/\{\{VERSION\}\}/$VERSION/" | sed -r "s/\{\{RELEASE\}\}/$RELEASE/" > sptk5.spec
+cd /build/$PACKAGE_NAME
 
-echo Copy sources to temp directory
-mkdir -p /tmp/$PACKAGE_NAME
-rsync -a . /tmp/$PACKAGE_NAME/
+cat /build/scripts/sptk.spec.src | sed -r "s/\{\{CORES\}\}/$CORES/" | sed -r "s/\{\{VERSION\}\}/$VERSION/" | sed -r "s/\{\{RELEASE\}\}/$RELEASE/" > sptk.spec
 
 echo Make source distribution archive
 CWD=`pwd`
-cd /tmp/$PACKAGE_NAME/
-rm -rf .git sptk5.tar.gz
 ./distclean.sh
 cd ..
-tar zcf $CWD/sptk5.tar.gz $PACKAGE_NAME
-cd $CWD
+tar zcf sptk-$VERSION.tar.gz $PACKAGE_NAME
 
 echo Prepare RPM build environment
 mkdir -p ~/rpmbuild/{SOURCES,RPMS/x86_64}/
-mv sptk5.tar.gz ~/rpmbuild/SOURCES/
+mv sptk-$VERSION.tar.gz ~/rpmbuild/SOURCES/
 rm ~/rpmbuild/RPMS/x86_64/sptk5*.rpm
 
-echo Build RPM
-rpmbuild -bb sptk5.spec
+ls -l ~/rpmbuild/SOURCES/
 
-cp ~/rpmbuild/RPMS/x86_64/sptk5*.rpm .
+echo Build RPM
+cd $CWD
+rpmbuild -bb sptk.spec
+
+cp ~/rpmbuild/RPMS/x86_64/sptk*.rpm .
 
