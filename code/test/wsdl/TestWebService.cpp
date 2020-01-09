f55ddb97047277becb90fb3f4efa8bd35f37ad75
Alexey Parshin <alexeyp@gmail.com>
2020-01-09 18:41:21 +1100

    WS unit tests development - in progress

---
+++ code/test/wsdl/TestWebService.cpp
@@ -0,0 +1,42 @@
+/*
+╔══════════════════════════════════════════════════════════════════════════════╗
+║                       SIMPLY POWERFUL TOOLKIT (SPTK)                         ║
+╟──────────────────────────────────────────────────────────────────────────────╢
+║  copyright            © 1999-2019 by Alexey Parshin. All rights reserved.    ║
+║  email                alexeyp@gmail.com                                      ║
+╚══════════════════════════════════════════════════════════════════════════════╝
+┌──────────────────────────────────────────────────────────────────────────────┐
+│   This library is free software; you can redistribute it and/or modify it    │
+│   under the terms of the GNU Library General Public License as published by  │
+│   the Free Software Foundation; either version 2 of the License, or (at your │
+│   option) any later version.                                                 │
+│                                                                              │
+│   This library is distributed in the hope that it will be useful, but        │
+│   WITHOUT ANY WARRANTY; without even the implied warranty of                 │
+│   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library   │
+│   General Public License for more details.                                   │
+│                                                                              │
+│   You should have received a copy of the GNU Library General Public License  │
+│   along with this library; if not, write to the Free Software Foundation,    │
+│   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.               │
+│                                                                              │
+│   Please report all bugs and problems to alexeyp@gmail.com.                  │
+└──────────────────────────────────────────────────────────────────────────────┘
+*/
+
+#include "TestWebService.h"
+
+using namespace std;
+using namespace sptk;
+
+void TestWebService::Hello(const CHello& input, CHelloResponse& output, HttpAuthentication* authentication)
+{
+    if (input.m_first_name.asString() != "John" || input.m_last_name.asString() != "Doe")
+        throw Exception("Invalid first or last name: expecting John Doe");
+    output.m_date_of_birth = DateTime("1981-02-01");
+    output.m_height = 6.5;
+    output.m_hour_rate = 15.6;
+    output.m_retired = false;
+    output.m_vacation_days = 21;
+    output.m_verified = DateTime("now");
+}
---
+++ code/test/wsdl/TestWebService.h
@@ -0,0 +1,42 @@
+/*
+╔══════════════════════════════════════════════════════════════════════════════╗
+║                       SIMPLY POWERFUL TOOLKIT (SPTK)                         ║
+╟──────────────────────────────────────────────────────────────────────────────╢
+║  copyright            © 1999-2019 by Alexey Parshin. All rights reserved.    ║
+║  email                alexeyp@gmail.com                                      ║
+╚══════════════════════════════════════════════════════════════════════════════╝
+┌──────────────────────────────────────────────────────────────────────────────┐
+│   This library is free software; you can redistribute it and/or modify it    │
+│   under the terms of the GNU Library General Public License as published by  │
+│   the Free Software Foundation; either version 2 of the License, or (at your │
+│   option) any later version.                                                 │
+│                                                                              │
+│   This library is distributed in the hope that it will be useful, but        │
+│   WITHOUT ANY WARRANTY; without even the implied warranty of                 │
+│   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library   │
+│   General Public License for more details.                                   │
+│                                                                              │
+│   You should have received a copy of the GNU Library General Public License  │
+│   along with this library; if not, write to the Free Software Foundation,    │
+│   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.               │
+│                                                                              │
+│   Please report all bugs and problems to alexeyp@gmail.com.                  │
+└──────────────────────────────────────────────────────────────────────────────┘
+*/
+
+#ifndef __TEST_WEB_SERVICE_H__
+#define __TEST_WEB_SERVICE_H__
+
+#include <test/wsdl/Service/CTestServiceBase.h>
+
+namespace sptk {
+
+class TestWebService : public CTestServiceBase
+{
+public:
+    void Hello(const CHello& input, CHelloResponse& output, sptk::HttpAuthentication* authentication) override;
+};
+
+}
+
+#endif
