69053707a24ee814065ee827e44914840c546fa1
Alexey Parshin <alexeyp@gmail.com>
2020-10-02 07:21:56 +1000

    MC

--- build/build_windows.bat
+++ build/build_windows.bat
@@ -1,5 +1,9 @@
 @echo off
 
+cd "C:\Users\alexe\workspace\sptk5"
+git reset --hard
+git pull
+
 cd "C:\Users\alexe\workspace\sptk5\build" 2>&1 > build.log
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" 2>&1 >> build.log
