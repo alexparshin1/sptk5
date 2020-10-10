22a2d86cca482cdfdac6637b5295083c45632e5e
Alexey Parshin <alexeyp@gmail.com>
2020-10-10 16:16:30 +1100

    MC

--- code/src/sputil/net/SSLSocket.cpp
+++ code/src/sputil/net/SSLSocket.cpp
@@ -220,7 +220,7 @@ void SSLSocket::openSocketFD(bool _blockingMode, const chrono::milliseconds& tim
     DateTime started = DateTime::Now();
     DateTime timeoutAt(started + timeout);
 
-    SSL_set_fd(m_ssl, fd());
+    SSL_set_fd(m_ssl, (int) fd());
 
     if (timeout == chrono::milliseconds(0)) {
         int rc = SSL_connect(m_ssl);
@@ -253,7 +253,7 @@ void SSLSocket::attach(SOCKET socketHandle, bool accept)
 
     if (fd() != socketHandle) {
         TCPSocket::attach(socketHandle, false);
-        int rc = SSL_set_fd(m_ssl, socketHandle);
+        int rc = SSL_set_fd(m_ssl, (int) socketHandle);
         if (rc <= 0) {
             int32_t errorCode = SSL_get_error(m_ssl, rc);
             string error = getSSLError("SSL_accept", errorCode);
