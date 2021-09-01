ceec0d7bb23861781d06667bca957baa8c8792e0
Alexey Parshin <alexeyp@gmail.com>
2021-09-02 08:49:31 +1000

    MC

--- code/src/sputil/core/FieldList.cpp
+++ code/src/sputil/core/FieldList.cpp
@@ -231,7 +231,7 @@ TEST(SPTK_FieldList, toXml)
     fieldsElement->exportTo(xdoc::DataFormat::XML, buffer, false);
 
     EXPECT_STREQ(buffer.c_str(),
-                 R"(<fields><name type="string" size="4">John</name><value type="int" size="4">12345</value></fields>)");
+                 R"(<fields><name>John</name><value>12345</value></fields>)");
 }
 
 #endif
