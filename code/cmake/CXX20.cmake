5017c866209940cc24dfc72cd190eb8a1510feed
Alexey Parshin <alexeyp@gmail.com>
2022-09-26 13:31:04 +1000

    Debugging fedora build

--- code/cmake/CXX20.cmake
+++ code/cmake/CXX20.cmake
@@ -1,10 +1,11 @@
 # Determines whether or not the compiler supports C++20
 macro(check_for_cxx20_compiler _VAR)
     #message(STATUS "Checking for C++20 compiler")
+    message(STATUS "Compiler version is ${CMAKE_CXX_COMPILER_VERSION}")
     set(${_VAR})
     if ((MSVC AND (MSVC12 OR MSVC13 OR MSVC14 OR MSVC15 OR MSVC16)) OR
-    (CMAKE_COMPILER_IS_GNUCXX AND NOT ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 5.0) OR
-    (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND NOT ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 5.0))
+    (CMAKE_COMPILER_IS_GNUCXX AND NOT ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 6.0) OR
+    (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND NOT ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 6.0))
         set(${_VAR} 1)
         #message(STATUS "Checking for C++20 compiler - available")
     else ()
