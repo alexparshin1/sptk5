96f794bdb802fb8b9c6e266c702137c5d4091c1e
Alexey Parshin <alexeyp@gmail.com>
2022-10-31 09:58:26 +1100

    Added CancellationToken

---
+++ code/sptk5/threads/CancellationToken.h
@@ -0,0 +1,53 @@
+/*
+╔══════════════════════════════════════════════════════════════════════════════╗
+║                       SIMPLY POWERFUL TOOLKIT (SPTK)                         ║
+╟──────────────────────────────────────────────────────────────────────────────╢
+║  copyright            © 1999-2022 Alexey Parshin. All rights reserved.       ║
+║  email                alexeyp@gmail.com                                      ║
+╚══════════════════════════════════════════════════════════════════════════════╝
+┌──────────────────────────────────────────────────────────────────────────────┐
+│   This library is free software; you can redistribute it and/or modify it    │
+│   under the terms of the GNU Library General Public License as published by  │
+│   the Free Software Foundation; either version 2 of the License, or (at your │
+│   option) any later version.                                                 │
+│                                                                              │
+│   This library is distributed in the hope that it will be useful, but        │
+│   WITHOUT ANY WARRANTY; without even the implied warranty of                 │
+│   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library   │
+│   General Public License for more details.                                   │
+│                                                                              │
+│   You should have received a copy of the GNU Library General Public License  │
+│   along with this library; if not, write to the Free Software Foundation,    │
+│   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.               │
+│                                                                              │
+│   Please report all bugs and problems to alexeyp@gmail.com.                  │
+└──────────────────────────────────────────────────────────────────────────────┘
+*/
+
+#pragma once
+
+#include "sptk5/cutils"
+
+namespace sptk {
+
+class CancellationToken
+{
+public:
+    void cancel()
+    {
+        std::scoped_lock lock(m_mutex);
+        m_value = false;
+    }
+
+    bool isCancelled() const
+    {
+        std::scoped_lock lock(m_mutex);
+        return m_value;
+    }
+
+private:
+    mutable std::mutex m_mutex;
+    bool m_value {false};
+};
+
+} // namespace sptk
