b064636eec471e5e4c1bf83fb46350eb60f22919
Alexey Parshin <alexeyp@gmail.com>
2023-03-24 19:25:41 +1100

    Debugging RPM build

--- build/build-package-cmake.sh
+++ build/build-package-cmake.sh
@@ -58,10 +58,10 @@ done
 ./distclean.sh
 
 export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64:/opt/oracle/instantclient_18_3:${LD_LIBRARY_PATH}
-echo "10.1.1.242  oracledb dbhost_oracle dbhost_mssql dbhost_pg dbhost_mysql" >> /etc/hosts
+echo "10.1.1.242  oracledb dbhost_oracle dbhost_mssql dbhost_pg dbhost_mysql smtp_host" >> /etc/hosts
 
 cat /etc/hosts
-cd test && /usr/local/bin/unit_tests 2>&1 > /build/farm/logs/unit_tests.$OS_TYPE.log  # --gtest_filter=SPTK_Oracle*
+cd test && /usr/local/bin/sptk_unit_tests 2>&1 > /build/farm/logs/unit_tests.$OS_TYPE.log  # --gtest_filter=SPTK_Oracle*
 RC=$?
 
 exit $RC
