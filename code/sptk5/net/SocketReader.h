173592d82223de8052998e021b5a320d59d51fc0
alexeyp <alexeyp@gmail.com>
2023-05-10 08:45:41 +1000

    Socket reader optimization and code reformat

--- code/sptk5/net/SocketReader.h
+++ code/sptk5/net/SocketReader.h
@@ -41,137 +41,156 @@ namespace sptk {
 /**
  * Thread-safe socket reader.
  */
-    class SP_EXPORT SocketReader
-            : public Buffer {
-    public:
-        /**
-         * Constructor
-         * @param socket            Socket to work with
-         * @param bufferSize        The desirable size of the internal buffer
-         */
-        explicit SocketReader(TCPSocket &socket, size_t bufferSize = 16384);
+class SP_EXPORT SocketReader
+    : public Buffer
+{
+public:
+    /**
+     * Constructor
+     * @param socket            Socket to work with
+     * @param bufferSize        The desirable size of the internal buffer
+     */
+    explicit SocketReader(TCPSocket& socket, size_t bufferSize = 16384);
 
-        /**
-         * Clears read buffer
-         */
-        virtual void clear();
+    /**
+     * Clears read buffer
+     */
+    virtual void clear();
 
-        /**
-         * Closes socket
-         */
-        virtual void close();
+    /**
+     * Closes socket
+     */
+    virtual void close();
 
-        /**
-         * Performs the buffered read
-         * @param destination       Destination buffer
-         * @param sz                Size of the destination buffer
-         * @returns bytes read from the internal buffer
-         */
-        size_t read(uint8_t *destination, size_t sz);
+    /**
+     * Performs the buffered read
+     * @param destination       Destination buffer
+     * @param sz                Size of the destination buffer
+     * @returns bytes read from the internal buffer
+     */
+    size_t read(uint8_t* destination, size_t sz);
 
-        /**
-         * Performs the buffered read
-         * @param destination       Destination buffer
-         * @param sz                Size of the destination buffer
-         * @returns bytes read from the internal buffer
-         */
-        size_t read(Buffer &destination, size_t sz);
+    /**
+     * Performs the buffered read
+     * @param destination       Destination buffer
+     * @param sz                Size of the destination buffer
+     * @returns bytes read from the internal buffer
+     */
+    size_t read(Buffer& destination, size_t sz);
 
-        /**
-         * Performs the buffered read
-         * @param destination       Destination buffer
-         * @param sz                Size of the destination buffer
-         * @returns bytes read from the internal buffer
-         */
-        template<typename T>
-        size_t read(T &destination) {
-            return read(&destination, sizeof(T));
-        }
+    /**
+     * Performs the buffered read
+     * @param destination       Destination buffer
+     * @param sz                Size of the destination buffer
+     * @returns bytes read from the internal buffer
+     */
+    template<typename T>
+    size_t read(T& destination)
+    {
+        return read(&destination, sizeof(T));
+    }
 
-        /**
-         * Performs the buffered read of LF-terminated data
-         * @param dest              Destination buffer
-         * @param delimiter         Line delimiter
-         * @returns bytes read from the internal buffer
-         */
-        size_t readLine(Buffer &dest, char delimiter = '\n');
+    /**
+     * Performs the buffered read of LF-terminated data
+     * @param dest              Destination buffer
+     * @param delimiter         Line delimiter
+     * @returns bytes read from the internal buffer
+     */
+    size_t readLine(Buffer& dest, char delimiter = '\n');
 
-        /**
-         * Performs the buffered read of LF-terminated string
-         * @param dest              Destination buffer
-         * @param delimiter         Line delimiter
-         * @returns bytes read from the internal buffer
-         */
-        size_t readLine(String &dest, char delimiter = '\n');
+    /**
+     * Performs the buffered read of LF-terminated string
+     * @param dest              Destination buffer
+     * @param delimiter         Line delimiter
+     * @returns bytes read from the internal buffer
+     */
+    size_t readLine(String& dest, char delimiter = '\n');
 
-        /**
-         * Return number of bytes available to read
-         */
-        [[nodiscard]] size_t availableBytes() const;
+    /**
+     * Return number of bytes available to read
+     */
+    [[nodiscard]] size_t availableBytes() const;
 
-        /**
-         * Check if reader has at least number of bytes available to read
-         * @param bytesToRead       Bytes to read
-         * @returns true if reader has at least number of bytes available to read
-         */
-        [[nodiscard]] bool canRead(size_t bytesToRead) const;
+    /**
+     * Check if reader has at least number of bytes available to read
+     * @param bytesToRead       Bytes to read
+     * @returns true if reader has at least number of bytes available to read
+     */
+    [[nodiscard]] bool canRead(size_t bytesToRead) const;
 
-        /**
-         * Return true if there are bytes available to read
-         * @param dest              Timeout waiting for data ready to read
-         */
-        [[nodiscard]] bool readyToRead(std::chrono::milliseconds timeout) const;
+    /**
+     * Return true if there are bytes available to read
+     * @param dest              Timeout waiting for data ready to read
+     */
+    [[nodiscard]] bool readyToRead(std::chrono::milliseconds timeout) const;
 
-        /**
-         * Return reader's socket
-         */
-        [[nodiscard]] TCPSocket &socket();
+    /**
+     * Return reader's socket
+     */
+    [[nodiscard]] TCPSocket& socket();
 
-        /**
-         *
-         * Return true if socket is active
-         */
-        [[nodiscard]] bool active() const {
-            return m_socket.active();
-        }
+    /**
+     *
+     * Return true if socket is active
+     */
+    [[nodiscard]] bool active() const
+    {
+        return m_socket.active();
+    }
 
-    private:
-        mutable std::mutex m_mutex; ///< Mutex protecting read operations
-        TCPSocket &m_socket;        ///< Socket to read from
-        uint32_t m_readOffset{0};  ///< Current offset in the read buffer
+private:
+    mutable std::mutex m_mutex; ///< Mutex protecting read operations
+    TCPSocket& m_socket;        ///< Socket to read from
+    size_t m_readOffset {0};    ///< Current offset in the read buffer
 
-        /**
-         * Performs the buffered read
-         * @param destination              Destination buffer
-         * @param size                Size of the destination buffer
-         * @param delimiter         Line delimiter
-         * @param read_line          True if we want to read one line (ended with CRLF) only
-         * @returns bytes read from the internal buffer
-         */
-        size_t read(uint8_t *destination, size_t size, char delimiter, bool read_line);
+    /**
+     * Performs buffered read
+     * @param destination       Destination buffer
+     * @param size              Size of the destination buffer
+     * @returns bytes read from the internal buffer
+     */
+    size_t read(uint8_t* destination, size_t size, char delimiter, bool read_line);
 
-        [[nodiscard]] int32_t readFromSocket();
+    /**
+     * Performs buffered read
+     * @param destination       Destination buffer
+     * @param size              Size of the destination buffer
+     * @param delimiter         Line delimiter
+     * @returns bytes read from the internal buffer
+     */
+    size_t readLine(uint8_t* destination, size_t size, char delimiter);
 
-        /**
-         * Performs buffered read
-         *
-         * Data is read from the opened socket into a character buffer of limited size
-         * @param destination       Destination buffer
-         * @param size                Size of the destination buffer
-         * @param delimiter         Line delimiter
-         * @param read_line          True if we want to read one line (ended with CRLF) only
-         * @returns number of bytes read
-         */
-        [[nodiscard]] int32_t bufferedRead(uint8_t *destination, size_t size, char delimiter, bool read_line);
+    [[nodiscard]] size_t readFromSocket();
 
-        /**
-         * Read more (as much as we can) from socket into buffer
-         * @param availableBytes    Number of bytes already available in buffer
-         */
-        void readMoreFromSocket(int availableBytes);
+    /**
+     * Performs buffered read
+     *
+     * Data is read from the opened socket into a buffer of limited size
+     * @param destination       Destination buffer
+     * @param size              Size of the destination buffer
+     * @returns number of bytes read
+     */
+    [[nodiscard]] size_t bufferedRead(uint8_t* destination, size_t size);
 
-        void handleReadFromSocketError(int error);
-    };
+    /**
+     * Performs buffered read
+     *
+     * Data is read from the opened socket into a character buffer of limited size
+     * @param destination       Destination buffer
+     * @param size              Size of the destination buffer
+     * @param delimiter         Line delimiter
+     * @returns number of bytes read
+     */
+    [[nodiscard]] int32_t bufferedReadLine(uint8_t* destination, size_t size, char delimiter);
+
+    /**
+     * Read more (as much as we can) from socket into buffer
+     * @param availableBytes    Number of bytes already available in buffer
+     */
+    void readMoreFromSocket(int availableBytes);
+
+    void handleReadFromSocketError(int error);
+};
 
 /**
  * @}
