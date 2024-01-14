9b7b02285e59095102e2dec2b2a81f5ee2ae624e
Alexey Parshin <alexeyp@gmail.com>
2024-01-14 18:22:54 +1100

    Started adding OCILib support

--- code/ocitest/ocitest.cpp
+++ code/ocitest/ocitest.cpp
@@ -1,9 +1,31 @@
+#include "ocilib.hpp"
 #include <iostream>
 
-using namespace std;
+using namespace ocilib;
 
 int main()
 {
-    cout << "Hello, World!" << endl;
-    return 0;
+    try
+    {
+        Environment::Initialize();
+
+        Connection con("theater:1521/xe", "gtest", "test#123");
+
+        Statement st(con);
+        st.Execute("select 1 from dual");
+
+        Resultset rs = st.GetResultset();
+        while (rs.Next())
+        {
+            std::cout << rs.Get<int>(1) << std::endl;
+        }
+    }
+    catch(std::exception &ex)
+    {
+        std::cout << ex.what() << std::endl;
+    }
+
+    Environment::Cleanup();
+
+    return EXIT_SUCCESS;
 }
