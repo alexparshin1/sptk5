820540f5ea9b7bf33fed98ae61cc272d43d88ac5
Alexey Parshin <alexeyp@gmail.com>
2024-05-26 10:30:47 +1000

    Updated CMake files

--- code/cmake/FindEPoll.cmake
+++ code/cmake/FindEPoll.cmake
@@ -1,47 +1,49 @@
 IF (WIN32)
 
-   SET (EPOLL_POSSIBLE_INCLUDE_PATHS
-        $ENV{SystemDrive}/wepoll/include
-        $ENV{SystemDrive}/*/wepoll/include
-        $ENV{ProgramFiles}/wepoll/include
-        $ENV{ProgramW6432}/wepoll/include)
+    SET(EPOLL_POSSIBLE_INCLUDE_PATHS
+            $ENV{SystemDrive}/wepoll/include
+            $ENV{SystemDrive}/*/wepoll/include
+            $ENV{ProgramFiles}/wepoll/include
+            $ENV{ProgramW6432}/wepoll/include)
 
-   SET (EPOLL_POSSIBLE_LIB_PATHS
-        $ENV{SystemDrive}/wepoll/lib
-        $ENV{SystemDrive}/*/wepoll/lib
-        $ENV{ProgramFiles}/wepoll/lib
-        $ENV{ProgramW6432}/wepoll/lib)
+    SET(EPOLL_POSSIBLE_LIB_PATHS
+            $ENV{SystemDrive}/wepoll/lib
+            $ENV{SystemDrive}/*/wepoll/lib
+            $ENV{ProgramFiles}/wepoll/lib
+            $ENV{ProgramW6432}/wepoll/lib)
 
-   SET (EPOLL_NAME wepoll)
+    SET(EPOLL_NAME wepoll)
 
-   FIND_PATH(EPOLL_INCLUDE_DIR wepoll.h ${EPOLL_POSSIBLE_INCLUDE_PATHS})
+    FIND_PATH(EPOLL_INCLUDE_DIR wepoll.h ${EPOLL_POSSIBLE_INCLUDE_PATHS})
 
-   FIND_LIBRARY(EPOLL_LIBRARY NAMES wepoll.lib PATHS ${EPOLL_POSSIBLE_LIB_PATHS})
+    FIND_LIBRARY(EPOLL_LIBRARY NAMES wepoll.lib PATHS ${EPOLL_POSSIBLE_LIB_PATHS})
 
 ELSE ()
 
-   SET (EPOLL_POSSIBLE_INCLUDE_PATHS /usr/include)
-   SET (EPOLL_POSSIBLE_LIB_PATHS /usr/lib /usr/local/lib)
-   FIND_PATH(EPOLL_INCLUDE_DIR sys/epoll.h ${EPOLL_POSSIBLE_INCLUDE_PATHS})
+    SET(EPOLL_POSSIBLE_INCLUDE_PATHS /usr/include)
+    SET(EPOLL_POSSIBLE_LIB_PATHS /usr/lib /usr/local/lib)
+    FIND_PATH(EPOLL_INCLUDE_DIR sys/epoll.h ${EPOLL_POSSIBLE_INCLUDE_PATHS})
 
-   IF (BSD)
-      FIND_LIBRARY(EPOLL_LIBRARY NAMES epoll PATHS ${EPOLL_POSSIBLE_LIB_PATHS})
-   ENDIF()
+    IF (BSD)
+        FIND_LIBRARY(EPOLL_LIBRARY NAMES epoll PATHS ${EPOLL_POSSIBLE_LIB_PATHS})
+    ELSE ()
+        SET(EPOLL_LIBRARY "")
+    ENDIF ()
 
-   SET (EPOLL_NAME epoll)
+    SET(EPOLL_NAME epoll)
 
 ENDIF ()
 
 IF (EPOLL_INCLUDE_DIR)
-   SET(EPOLL_FOUND TRUE)
+    SET(EPOLL_FOUND TRUE)
 ENDIF ()
 
 IF (EPOLL_FOUND)
-   IF (NOT EPoll_FIND_QUIETLY)
-      MESSAGE(STATUS "Found EPoll:  ${EPOLL_LIBRARY}")
-   ENDIF ()
+    IF (NOT EPoll_FIND_QUIETLY)
+        MESSAGE(STATUS "Found EPoll:  ${EPOLL_LIBRARY}")
+    ENDIF ()
 ELSE ()
-   IF (EPoll_FIND_REQUIRED)
-      MESSAGE(FATAL_ERROR "Could not find ${EPOLL_NAME} library")
-   ENDIF ()
+    IF (EPoll_FIND_REQUIRED)
+        MESSAGE(FATAL_ERROR "Could not find ${EPOLL_NAME} library")
+    ENDIF ()
 ENDIF ()
--- code/cmake/FindPostgreSQL.cmake
+++ code/cmake/FindPostgreSQL.cmake
@@ -1,68 +1,68 @@
 IF (WIN32)
-   SET (PGSQL_POSSIBLE_INCLUDE_PATHS
-        $ENV{SystemDrive}/PostgreSQL/*/include
-        $ENV{ProgramFiles}/*/include
-        $ENV{ProgramFiles}/PostgreSQL/*/include
-        $ENV{ProgramW6432}/*/include
-        $ENV{ProgramW6432}/PostgreSQL/*/include)
-   SET (PGSQL_POSSIBLE_LIB_PATHS
-        $ENV{SystemDrive}/PostgreSQL/*/lib
-        $ENV{ProgramFiles}/*/lib
-        $ENV{ProgramFiles}/PostgreSQL/*/lib
-        $ENV{ProgramW6432}/*/lib
-        $ENV{ProgramW6432}/PostgreSQL/*/lib)
-   FIND_PATH(PostgreSQL_INCLUDE_DIR libpq-fe.h ${PGSQL_POSSIBLE_INCLUDE_PATHS})
-   FIND_LIBRARY(PostgreSQL_LIBRARY NAMES libpq pq PATHS ${PGSQL_POSSIBLE_LIB_PATHS})
+    SET(PGSQL_POSSIBLE_INCLUDE_PATHS
+            $ENV{SystemDrive}/PostgreSQL/*/include
+            $ENV{ProgramFiles}/*/include
+            $ENV{ProgramFiles}/PostgreSQL/*/include
+            $ENV{ProgramW6432}/*/include
+            $ENV{ProgramW6432}/PostgreSQL/*/include)
+    SET(PGSQL_POSSIBLE_LIB_PATHS
+            $ENV{SystemDrive}/PostgreSQL/*/lib
+            $ENV{ProgramFiles}/*/lib
+            $ENV{ProgramFiles}/PostgreSQL/*/lib
+            $ENV{ProgramW6432}/*/lib
+            $ENV{ProgramW6432}/PostgreSQL/*/lib)
+    FIND_PATH(PostgreSQL_INCLUDE_DIR libpq-fe.h ${PGSQL_POSSIBLE_INCLUDE_PATHS})
+    FIND_LIBRARY(PostgreSQL_LIBRARY NAMES libpq pq PATHS ${PGSQL_POSSIBLE_LIB_PATHS})
 
 ELSE (WIN32)
 
-   SET (PGSQL_POSSIBLE_INCLUDE_PATHS
-        /usr/include
-        /usr/*/include
-        /usr/include/*
-        /usr/local/include
-        /usr/local/*/include
-        /usr/local/include/*)
-   SET (PGSQL_POSSIBLE_LIB_PATHS
-        /usr/lib
-        /usr/*/lib
-        /usr/local/lib
-        /usr/local/*/lib)
+    SET(PGSQL_POSSIBLE_INCLUDE_PATHS
+            /usr/include
+            /usr/*/include
+            /usr/include/*
+            /usr/local/include
+            /usr/local/*/include
+            /usr/local/include/*)
+    SET(PGSQL_POSSIBLE_LIB_PATHS
+            /usr/lib
+            /usr/*/lib
+            /usr/local/lib
+            /usr/local/*/lib)
 
-   FIND_PROGRAM (PGSQL_CONFIG pg_config PATHS /usr/bin /usr/postgres/*/bin /usr/local/bin)
-   IF (PGSQL_CONFIG)
-      EXEC_PROGRAM ("${PGSQL_CONFIG}" ARGS --includedir OUTPUT_VARIABLE PostgreSQL_INCLUDE_DIR)
-      EXEC_PROGRAM ("${PGSQL_CONFIG}" ARGS --libdir OUTPUT_VARIABLE PostgreSQL_LIB_DIR)
-   ELSE ()
-      FIND_PATH(PostgreSQL_INCLUDE_DIR libpq-fe.h ${PGSQL_POSSIBLE_INCLUDE_PATHS})
-   ENDIF ()
-   FIND_LIBRARY(PostgreSQL_LIBRARY NAMES pq PATHS "${PostgreSQL_LIB_DIR}" ${PGSQL_POSSIBLE_LIB_PATHS})
+    FIND_PROGRAM(PGSQL_CONFIG pg_config PATHS /usr/bin /usr/postgres/*/bin /usr/local/bin)
+    IF (PGSQL_CONFIG)
+        EXECUTE_PROCESS(COMMAND "${PGSQL_CONFIG}" --includedir OUTPUT_VARIABLE PostgreSQL_INCLUDE_DIR)
+        EXECUTE_PROCESS(COMMAND "${PGSQL_CONFIG}" --libdir OUTPUT_VARIABLE PostgreSQL_LIB_DIR)
+    ELSE ()
+        FIND_PATH(PostgreSQL_INCLUDE_DIR libpq-fe.h ${PGSQL_POSSIBLE_INCLUDE_PATHS})
+    ENDIF ()
+    FIND_LIBRARY(PostgreSQL_LIBRARY NAMES pq PATHS "${PostgreSQL_LIB_DIR}" ${PGSQL_POSSIBLE_LIB_PATHS})
 ENDIF (WIN32)
 
 IF (PostgreSQL_INCLUDE_DIR AND PostgreSQL_LIBRARY)
-   SET(PostgreSQL_FOUND TRUE)
+    SET(PostgreSQL_FOUND TRUE)
 ENDIF (PostgreSQL_INCLUDE_DIR AND PostgreSQL_LIBRARY)
 
 IF (PostgreSQL_FOUND)
-   IF (NOT PostgreSQL_FIND_QUIETLY)
-      MESSAGE(STATUS "Found PostgreSQL: ${PostgreSQL_LIBRARY}")
-   ENDIF (NOT PostgreSQL_FIND_QUIETLY)
+    IF (NOT PostgreSQL_FIND_QUIETLY)
+        MESSAGE(STATUS "Found PostgreSQL: ${PostgreSQL_LIBRARY}")
+    ENDIF (NOT PostgreSQL_FIND_QUIETLY)
 ELSE (PostgreSQL_FOUND)
-   IF (PostgreSQL_FIND_REQUIRED)
-      MESSAGE(FATAL_ERROR "Could not find PostgreSQL")
-   ENDIF (PostgreSQL_FIND_REQUIRED)
+    IF (PostgreSQL_FIND_REQUIRED)
+        MESSAGE(FATAL_ERROR "Could not find PostgreSQL")
+    ENDIF (PostgreSQL_FIND_REQUIRED)
 ENDIF (PostgreSQL_FOUND)
 
 IF (PostgreSQL_INCLUDE_DIR AND PostgreSQL_LIBRARY)
-   SET(PGSQL_FOUND TRUE)
+    SET(PGSQL_FOUND TRUE)
 ENDIF (PostgreSQL_INCLUDE_DIR AND PostgreSQL_LIBRARY)
 
 IF (PGSQL_FOUND)
-   IF (NOT PostgreSQL_FIND_QUIETLY)
-      MESSAGE(STATUS "Found PostgreSQL: ${PostgreSQL_LIBRARY}")
-   ENDIF (NOT PostgreSQL_FIND_QUIETLY)
+    IF (NOT PostgreSQL_FIND_QUIETLY)
+        MESSAGE(STATUS "Found PostgreSQL: ${PostgreSQL_LIBRARY}")
+    ENDIF (NOT PostgreSQL_FIND_QUIETLY)
 ELSE (PGSQL_FOUND)
-   IF (PostgreSQL_FIND_REQUIRED)
-      MESSAGE(FATAL_ERROR "Could not find PostgreSQL")
-   ENDIF (PostgreSQL_FIND_REQUIRED)
+    IF (PostgreSQL_FIND_REQUIRED)
+        MESSAGE(FATAL_ERROR "Could not find PostgreSQL")
+    ENDIF (PostgreSQL_FIND_REQUIRED)
 ENDIF (PGSQL_FOUND)
