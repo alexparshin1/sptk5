5ac704182d55479cd8e35f377075264766368389
Alexey Parshin <alexeyp@gmail.com>
2024-11-13 20:27:34 +1100

    Development of site-react: Added Advanced installer credits.

--- site-react/src/pages/Header.jsx
+++ site-react/src/pages/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../images/logo.png";
 import linotex from "../images/linotex_white_1.png";
+import advancedInstaller from "../images/AdvancedInstallerLogo.png";
 import "../css/Header.css";
 
 export default class Header extends React.Component
@@ -15,10 +16,25 @@ export default class Header extends React.Component
             <table style={{width: "100%"}}>
                 <tbody>
                 <tr>
-                    <td align="left"><img src={logo} alt=""/></td>
-                    <td align="right"><img src={linotex} width="205" height="59" alt=""/>
+                    <td align="left" colSpan={2}><img src={logo} alt=""/></td>
+                    <td align="right">
+                        <img src={linotex} width="205" height="59" alt=""/>
+                    </td>
+                    <td align="right" width="250">
+                        <img src={advancedInstaller} width="205" height="59" alt=""/>
                     </td>
                     <td width="20"></td>
+                </tr>
+                <tr className="Credits">
+                    <td>Credits:</td>
+                    <td>
+                        The SPTK web site is provided by
+                        Linotex.
+                    </td>
+                    <td colSpan="2">
+                        The SPTK Windows installer is created with Advanced
+                        Installer free Open-Source license.
+                    </td>
                 </tr>
                 </tbody>
             </table>
