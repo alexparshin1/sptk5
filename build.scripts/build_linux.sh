b0ba77edf6989731f0975d2070750682b59806b6
Alexey Parshin <alexeyp@gmail.com>
2025-05-18 21:22:44 +1000

    Updated build script

--- build.scripts/build_linux.sh
+++ build.scripts/build_linux.sh
@@ -16,25 +16,29 @@ fi
 SPTK_DIR=SPTK-$SPTK_VERSION
 XMQ_DIR=XMQ-$XMQ_VERSION
 
-sudo rm -rf $SPTK_DIR $XMQ_DIR
+echo "$(date +%H:%M:%S) Remove build directories: $SPTK_DIR $XMQ_DIR"
+sudo rm -rf $SPTK_DIR $XMQ_DIR > /tmp/op.log || (cat /tmp/op.log; exit 1)
 
+echo "$(date +%H:%M:%S) Update SPTK from git"
 cd $BUILD_ROOT/git/sptk5/
-git pull > /dev/null
-git checkout $SPTK_VERSION || exit 1
+git pull  > /tmp/op.log || (cat /tmp/op.log; exit 1)
+git checkout $SPTK_VERSION > /tmp/op.log || (cat /tmp/op.log; exit 1)
 
+echo "$(date +%H:%M:%S) Update XMQ from git"
 cd $BUILD_ROOT/git/xmq
-git pull > /dev/null
-git checkout $XMQ_VERSION || exit 1
+git pull > /tmp/op.log || (cat /tmp/op.log; exit 1)
+git checkout $XMQ_VERSION > /tmp/op.log || (cat /tmp/op.log; exit 1)
 
+echo "$(date +%H:%M:%S) Create build directories: $SPTK_DIR $XMQ_DIR"
 cd $BUILD_ROOT
-rsync -av git/sptk5/code/ $SPTK_DIR > /dev/null
-rsync -av git/xmq/ $XMQ_DIR > /dev/null
+rsync -av git/sptk5/code/ $SPTK_DIR > /tmp/op.log || (cat /tmp/op.log; exit 1)
+rsync -av git/xmq/ $XMQ_DIR > /tmp/op.log || (cat /tmp/op.log; exit 1)
 
 rm -f logs/*.log
 
-for dname in /home/alexeyp/Docker/Dockerfile.*
+#for dname in /home/alexeyp/Docker/Dockerfile.*
 #for dname in /home/alexeyp/Docker/Dockerfile.ubuntu-25.10
-#for dname in /home/alexeyp/Docker/Dockerfile.oraclelinux-9.5
+for dname in /home/alexeyp/Docker/Dockerfile.oraclelinux-9.5
 do
     name=$(echo $dname | sed -re 's/^.*Dockerfile.//')
     echo "$(date +%H:%M:%S) Building $name"
