7e7d546041e96fcd3c572a818e30141d6f5e4f84
Alexey Parshin <alexeyp@gmail.com>
2026-06-18 14:14:42 +1000

    update file permission

---
+++ code/installer/spinst.cpp
@@ -0,0 +1,53 @@
+/*
+╔══════════════════════════════════════════════════════════════════════════════╗
+║                       SIMPLY POWERFUL TOOLKIT (SPTK)                         ║
+║                       spell_checker.cpp - description                        ║
+╟──────────────────────────────────────────────────────────────────────────────╢
+║  begin                Thursday May 25 2000                                   ║
+║  copyright            © 1999-2026 Alexey Parshin. All rights reserved.       ║
+║  email                alexeyp@gmail.com                                      ║
+╚══════════════════════════════════════════════════════════════════════════════╝
+┌──────────────────────────────────────────────────────────────────────────────┐
+│   This library is free software; you can redistribute it and/or modify it    │
+│   under the terms of the GNU Library General Public License as published by  │
+│   the Free Software Foundation; either version 2 of the License, or (at your │
+│   option) any later version.                                                 │
+│                                                                              │
+│   This library is distributed in the hope that it will be useful, but        │
+│   WITHOUT ANY WARRANTY; without even the implied warranty of                 │
+│   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library   │
+│   General Public License for more details.                                   │
+│                                                                              │
+│   You should have received a copy of the GNU Library General Public License  │
+│   along with this library; if not, write to the Free Software Foundation,    │
+│   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.               │
+│                                                                              │
+│   Please report all bugs and problems to alexeyp@gmail.com.                  │
+└──────────────────────────────────────────────────────────────────────────────┘
+*/
+
+#include <sptk5/cgui>
+#include <sptk5/gui/CEditorSpellChecker.h>
+
+using namespace std;
+using namespace sptk;
+
+int main()
+{
+    CThemes themes;
+    CWindow window(600, 400, "SP Installer");
+
+    CToolBar toolBar;
+    CButton  spellCheckButton("Prior", CLayoutAlign::RIGHT);
+    CButton  spellCheckButton("Next", CLayoutAlign::RIGHT);
+
+    window.show();
+
+    CThemes::set("OSX");
+
+    window.reLayout();
+
+    Fl::run();
+
+    return 0;
+}
