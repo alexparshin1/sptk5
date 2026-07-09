b219399f69cb8283aac63fa696618db165c10cac
Alexey Parshin <alexeyp@gmail.com>
2026-07-09 21:25:54 +1000

    Generated change list

--- code/generate_change_list.sh
+++ code/generate_change_list.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-git log --since $1 --pretty=format:%s | grep -vE '(MC|Intermediate|Merge|Code cleanup|Debugging|Version)' | grep -vE '(Doxygen|documentation|compilation|missing|CMake)'
+echo '<ul>' > change_list.txt
+git log --since $1 --pretty=format:%s \
+  | grep -vEi '(MC|Intermediate|Merge|Code cleanup|Debugging|Working|Version|optimizing|updating)' \
+  | grep -vEi '(Doxygen|documentation|optimization|optimisation|compilation|build fix|missing|CMake|unit test)' \
+  | grep -vE '^(Refactor|Review|Fixed|Code review|Optimized|comments)' \
+  | grep -vE '^Updated.*class\.?' \
+  | sort -u \
+  | sed -re 's|^(.*)$|  <li>\1.</li>|;s|\.\.|.|' >> change_list.txt
+echo '</ul>'>> change_list.txt
