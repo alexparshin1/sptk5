99fa9e87482c7e81d48b5c81c55ea0393ec12afa
Alexey Parshin <alexeyp@gmail.com>
2026-07-15 19:48:29 +1000

    Debugging build

--- build.scripts/build-package-cmake.sh
+++ build.scripts/build-package-cmake.sh
@@ -84,7 +84,7 @@ fi
 
 sh ./distclean.sh
 ulimit -n 16384
-cmake . $BUILD_OPTIONS && make -j6 install && make -j6 package || exit 1
+cmake . $BUILD_OPTIONS -DCMAKE_BUILD_TYPE=Release && make -j6 install && make -j6 package || exit 1
 
 echo ──────────────────────────────────────────────────────────────────
 BUILD_OUTPUT_DIR=/build/output/$PACKAGE-$VERSION
