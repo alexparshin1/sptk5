763c90b97fcd3d3f26bd0434a27384a16bcba7df
Alexey Parshin <alexeyp@gmail.com>
2026-09-26 16:18:17 +1000

    Debugging Windows build

--- code/themes/tar_theme.bat
+++ code/themes/tar_theme.bat
@@ -4,4 +4,4 @@ SET THEME=%~1
 SET THEME_DIR=%~2
 echo Theme directory: %THEME_DIR%
 cd %THEME_DIR%
-tar cf %THEME%.tar -C %THEME_DIR% %~3 %~4 %~5 %~6 %~7 %~8
+tar cf ..\%THEME%.tar -C %THEME_DIR% %~3 %~4 %~5 %~6 %~7 %~8
