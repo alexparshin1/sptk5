411cda1f0505cc65af6f1cd6eac94710a4621aa6
Alexey Parshin <alexeyp@gmail.com>
2026-09-26 16:25:05 +1000

    Debugging Windows build

--- code/themes/tar_theme.bat
+++ code/themes/tar_theme.bat
@@ -8,4 +8,4 @@ echo Current directory: %CWD%
 echo Theme directory:   %THEME_DIR%
 
 cd %THEME_DIR%
-tar cf ..\%THEME%.tar -C %THEME_DIR% %~3 %~4 %~5 %~6 %~7 %~8
+tar cf %CWD%\%THEME%.tar -C %THEME_DIR% %~3 %~4 %~5 %~6 %~7 %~8
